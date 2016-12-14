c1f5bf095791925673b53e1e3c6d8335c2f5cc35
Gustavo/Lucas <guguandrade_08@hotmail.com>
2016-12-14 16:55:31 -0200

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlalchemy
 import MySQLdb
 from sqlalchemy import create_engine
 
-parametros_conexao='mysql://Guluad:Batata@123@Guluad.mysql.pythonanywhere-services.com/Guluad$PyTwitter?charset=utf8'
+parametros_conexao='[endereço_do_banco]'
 engine = create_engine(parametros_conexao)
 conn = engine.connect()
 tabela='tweets'
